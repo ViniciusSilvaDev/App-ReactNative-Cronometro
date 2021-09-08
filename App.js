64e35d642c7abe764ac2386afb1f592ffb74beba
Vinicius Silva <vinicius.silva277@hotmail.com>
2021-09-08 18:04:40 -0300

    Commit front
    Foi inserido o SafeAreaView, a StatusBar com a cor predominante, o Icone do cronometro e o Title para uma experiência mais agradável

--- App.js
+++ App.js
@@ -1,138 +1,159 @@
-import React, { Component } from 'react';
-import {
+import React, {Component} from 'react';
+import{
   View,
   Text,
   StyleSheet,
   Image,
-  TouchableOpacity
-  } from 'react-native';
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
 
 class App extends Component{
 
-  constructor(props){
-    super(props);
-    this.state = {
-      numero: 0,
-      botao: 'VAI',
-      ultimo: null
-    };
+ constructor(props){
+   super(props);
+   this.state={
+     numero:0,
+     botao:'ATIVAR',
+     botao2:'ZERAR',
+     ultimo:null
+   };
 
-    //Variavel do timer do relogio.
-    this.timer = null;
-
-    this.vai = this.vai.bind(this);
-    this.limpar = this.limpar.bind(this);
+   this.timer=null;   //variavel para controlar o timer ativo ou parado
+   this.vai= this.vai.bind(this);
+   this.limpar= this.limpar.bind(this);
   }
+ vai(){
 
-  vai(){
-
-    if(this.timer != null){
-      //Aqui vai parar o timer
+    if(this.timer!=null){ 
+      //aqui vai parar o timer 
       clearInterval(this.timer);
-      this.timer = null;
-
-      this.setState({botao: 'VAI'});
+      this.timer=null;
+      this.setState({botao:'ATIVAR'});
     }else{
-
-      //Comeca girar o timer
-      this.timer = setInterval( ()=> {
-        this.setState({numero: this.state.numero + 0.1})
-      }, 100);
-
-      this.setState({botao: 'PARAR'});
-    }
-
+      this.timer= setInterval(() => {
+      this.setState({numero:this.state.numero + 0.1})
+       }, 100);
+       this.setState({botao:'PAUSAR'});
+    }   
   }
-
-  limpar(){
-    if(this.timer != null){
-      //Aqui vai parar o timer
+ limpar(){
+    if (this.timer!=null){
+      //aqui vai zerar o timer
       clearInterval(this.timer);
-      this.timer = null;
+      this.timer=null;
     }
     this.setState({
       ultimo: this.state.numero,
-      numero: 0,
-      botao: 'VAI'
+      numero:0,
+      botao:'ATIVAR'
     })
-  }
+
+ }
+
 
   render(){
     return(
-      <View style={styles.container}>  
+      <SafeAreaView style ={styles.areaContainer}>
 
-      <Image
-      source={require('./src/cronometro.png')}
-      style={styles.cronometro}
-      />
+        <StatusBar
+        animated={true}
+        backgroundColor="#FF4500"
+        barStyle="light-content"
+        showHideTransition="fade"
+        hidden={false}
+        />
 
-      <Text style={styles.timer}> {this.state.numero.toFixed(1)} </Text>
+        <Text style={{textAlign:'center', fontWeight:'bold', fontSize:45, color:"#000000", marginBottom: 10 }}>Stopwatch</Text>
 
-      <View style={styles.btnArea}>
+        <View style={styles.areaImg} >
+          <Image 
+            source={require('./src/Cronometro.png')}
+            style={styles.cronometro}
+          />
+        </View>
+      
+       <Text style={styles.timer}> {this.state.numero.toFixed(1)} </Text>
 
-        <TouchableOpacity style={styles.btn} onPress={this.vai}>
+       <View style={styles.btnArea}> 
+
+        <TouchableOpacity style={styles.btn} onPress={this.vai} >
           <Text style={styles.btnTexto}> {this.state.botao} </Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.btn} onPress={this.limpar}>
-          <Text style={styles.btnTexto}>LIMPAR</Text>
+        <TouchableOpacity style={styles.btn}onPress={this.limpar}>
+          <Text style={styles.btnTexto}> {this.state.botao2} </Text>
         </TouchableOpacity>
+      
+       </View>
 
-      </View>
-
-      <View style={styles.areaUltima}>
-          <Text style={styles.textoCorrida}>
-            {this.state.ultimo > 0 ? 'Ultimo tempo: ' + this.state.ultimo.toFixed(2) + 's' : ''}
+       <View style={styles.areaUltima}>
+          <Text style={styles.textoUltimo}> 
+          {this.state.ultimo> 0 ? 'Ultimo tempo:  ' + this.state.ultimo.toFixed(2) +'s': ''}
           </Text>
-      </View>
+        </View>
 
-
-      </View>    
+      </SafeAreaView>
     );
   }
-
 }
 
-const styles = StyleSheet.create({
-  container:{
+const styles= StyleSheet.create({
+  areaContainer:{
     flex:1,
     alignItems:'center',
-    justifyContent: 'center',
-    backgroundColor: '#DAA520'
+    justifyContent:'center',
+    backgroundColor:'#FF4500'
   },
+
+  areaImg:{
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+
+  cronometro:{
+    width: 300,
+    height: 310,
+  },
+
   timer:{
-    marginTop:-160,
-    color: '#FFF',
-    fontSize: 65,
-    fontWeight: 'bold'
+    marginTop:-180,
+    color:'#fff',
+    fontSize:65,
+    fontWeight:'bold',
   },
+
   btnArea:{
     flexDirection: 'row',
-    marginTop: 70,
-    height: 40
+    marginTop:70,
+    height:40
   },
   btn:{
     flex:1,
     justifyContent:'center',
-    alignItems: 'center',
-    backgroundColor: '#FFF',
-    height: 40,
-    margin: 17,
-    borderRadius: 9
+    alignItems:'center',
+    backgroundColor:'#fff',
+    height:40,
+    margin:25,
+    borderRadius:15
   },
   btnTexto:{
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#DAA520'
+    fontSize:20,
+    fontWeight:'bold',
+    color:'#000000'
   },
   areaUltima:{
-    marginTop: 40,
+    marginTop:40,
   },
-  textoCorrida:{
+  textoUltimo:{
+    fontWeight:'bold',
     fontSize:25,
     fontStyle:'italic',
-    color: '#FFF'
+    color:'#000000'
   }
+
 });
 
 export default App;
